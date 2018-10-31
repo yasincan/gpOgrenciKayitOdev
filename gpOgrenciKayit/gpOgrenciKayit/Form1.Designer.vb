820bfbb04b437199ca7e3b81159d9e34d19905aa
Yasin Can <yasincan.cs@outlook.com>
2018-10-31 22:23:24 +0300

    'ok'

---
+++ gpOgrenciKayit/gpOgrenciKayit/Form1.Designer.vb
@@ -0,0 +1,189 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.ButtonAdd = New System.Windows.Forms.Button()
+        Me.stName = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.stSurname = New System.Windows.Forms.TextBox()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.stNumber = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.ButtonDeleted = New System.Windows.Forms.Button()
+        Me.ButtonUpdated = New System.Windows.Forms.Button()
+        Me.stClass = New System.Windows.Forms.TextBox()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 242)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(552, 206)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'ButtonAdd
+        '
+        Me.ButtonAdd.Location = New System.Drawing.Point(19, 135)
+        Me.ButtonAdd.Name = "ButtonAdd"
+        Me.ButtonAdd.Size = New System.Drawing.Size(75, 23)
+        Me.ButtonAdd.TabIndex = 1
+        Me.ButtonAdd.Text = "Kaydet"
+        Me.ButtonAdd.UseVisualStyleBackColor = True
+        '
+        'stName
+        '
+        Me.stName.Location = New System.Drawing.Point(104, 16)
+        Me.stName.Name = "stName"
+        Me.stName.Size = New System.Drawing.Size(100, 20)
+        Me.stName.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(16, 19)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(65, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Ögrenci Adı:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(16, 45)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(82, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Ögrenci Soyadı:"
+        '
+        'stSurname
+        '
+        Me.stSurname.Location = New System.Drawing.Point(104, 42)
+        Me.stSurname.Name = "stSurname"
+        Me.stSurname.Size = New System.Drawing.Size(100, 20)
+        Me.stSurname.TabIndex = 4
+        '
+        'Panel1
+        '
+        Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel1.Controls.Add(Me.stClass)
+        Me.Panel1.Controls.Add(Me.ButtonUpdated)
+        Me.Panel1.Controls.Add(Me.ButtonDeleted)
+        Me.Panel1.Controls.Add(Me.ButtonAdd)
+        Me.Panel1.Controls.Add(Me.Label4)
+        Me.Panel1.Controls.Add(Me.stNumber)
+        Me.Panel1.Controls.Add(Me.Label3)
+        Me.Panel1.Controls.Add(Me.stSurname)
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Controls.Add(Me.stName)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Location = New System.Drawing.Point(12, 12)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(294, 192)
+        Me.Panel1.TabIndex = 6
+        '
+        'stNumber
+        '
+        Me.stNumber.Location = New System.Drawing.Point(104, 68)
+        Me.stNumber.Name = "stNumber"
+        Me.stNumber.Size = New System.Drawing.Size(100, 20)
+        Me.stNumber.TabIndex = 6
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(16, 71)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(64, 13)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Ögrenci No:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(16, 97)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(72, 13)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "Ögrenci Sınıfı:"
+        '
+        'ButtonDeleted
+        '
+        Me.ButtonDeleted.Location = New System.Drawing.Point(104, 135)
+        Me.ButtonDeleted.Name = "ButtonDeleted"
+        Me.ButtonDeleted.Size = New System.Drawing.Size(75, 23)
+        Me.ButtonDeleted.TabIndex = 10
+        Me.ButtonDeleted.Text = "Sil"
+        Me.ButtonDeleted.UseVisualStyleBackColor = True
+        '
+        'ButtonUpdated
+        '
+        Me.ButtonUpdated.Location = New System.Drawing.Point(185, 135)
+        Me.ButtonUpdated.Name = "ButtonUpdated"
+        Me.ButtonUpdated.Size = New System.Drawing.Size(75, 23)
+        Me.ButtonUpdated.TabIndex = 11
+        Me.ButtonUpdated.Text = "Güncelle"
+        Me.ButtonUpdated.UseVisualStyleBackColor = True
+        '
+        'stClass
+        '
+        Me.stClass.Location = New System.Drawing.Point(104, 97)
+        Me.stClass.Name = "stClass"
+        Me.stClass.Size = New System.Drawing.Size(100, 20)
+        Me.stClass.TabIndex = 12
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(578, 458)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Name = "Form1"
+        Me.Text = "Ögerenci Kayit"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents ButtonAdd As Button
+    Friend WithEvents stName As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents stSurname As TextBox
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Label4 As Label
+    Friend WithEvents stNumber As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents ButtonDeleted As Button
+    Friend WithEvents ButtonUpdated As Button
+    Friend WithEvents stClass As TextBox
+End Class
---
+++ gpOgrenciKayit/gpOgrenciKayit/Form1.vb
@@ -0,0 +1,85 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Form1
+    Public con As New SqlConnection("Initial Catalog=gpOgrenciKayit;Data Source=localhost;Integrated Security=SSPI;")
+    Dim cmd As New SqlCommand
+    Dim adtr As New SqlDataAdapter("select * From Students", con)
+    Dim dtst As New DataSet
+    Private Sub show()
+        dtst.Clear()
+        adtr.Fill(dtst, "st")
+        DataGridView1.DataSource = dtst.Tables("st")
+    End Sub
+    Private Sub clear()
+        stName.Clear()
+        stSurname.Clear()
+        stNumber.Clear()
+        stClass.Clear()
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Call show()
+    End Sub
+    Dim Id As Integer
+    Private Sub DataGridView1_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellClick
+        Try
+            Dim s As Integer
+            s = DataGridView1.CurrentCell.RowIndex
+            Id = DataGridView1(0, s).Value
+            stName.Text = DataGridView1(1, s).Value
+            stSurname.Text = DataGridView1(2, s).Value
+            stNumber.Text = DataGridView1(3, s).Value
+            stClass.Text = DataGridView1(4, s).Value
+        Catch ex As Exception
+
+        End Try
+    End Sub
+    Private Sub ButtonAdd_Click(sender As Object, e As EventArgs) Handles ButtonAdd.Click
+        If Me.stNumber.Text = "" Then
+            MsgBox("Lütfen Tüm Alanını Doldurunuz. Alan Boş Bırakılamaz.", MsgBoxStyle.Exclamation, "Kayıt Uyarı")
+        Else
+            cmd.Connection = con
+            cmd.CommandText = "Insert into Students(stName,stSurname,stNumber,stClass) values (@stName,@stSurname,@stNumber,@stClass)"
+            cmd.Parameters.AddWithValue("@stName", stName.Text)
+            cmd.Parameters.AddWithValue("@stSurname", stSurname.Text)
+            cmd.Parameters.AddWithValue("@stNumber", stNumber.Text)
+            cmd.Parameters.AddWithValue("@stClass", stClass.Text)
+            con.Open()
+            cmd.ExecuteNonQuery()
+            con.Close()
+            MsgBox("Girdiğiniz Ögrenci Kayıt Yapılmıştır.", MsgBoxStyle.Information, "Tebrikler")
+            Call clear()
+            Call show()
+        End If
+    End Sub
+    Private Sub ButtonDeleted_Click(sender As Object, e As EventArgs) Handles ButtonDeleted.Click
+        If Me.stName.Text = "" Then
+            MsgBox("Lütfen Kayıt Seçiniz.", MsgBoxStyle.Exclamation, "DİKKAT")
+        Else
+            cmd.Connection = con
+            cmd.CommandText = "DELETE FROM Students WHERE Id='" & Id & "'"
+            con.Open()
+            cmd.ExecuteNonQuery()
+            con.Close()
+            MsgBox("Listeden Seçmiş Olduğunuz Veriler Silinmiştir.", MsgBoxStyle.Exclamation, "Silme İşlemi")
+            Call clear()
+            Call show()
+        End If
+    End Sub
+
+    Private Sub ButtonUpdated_Click(sender As Object, e As EventArgs) Handles ButtonUpdated.Click
+        If Me.stName.Text = "" Then
+            MsgBox("Lütfen Kayıt Seçiniz.", MsgBoxStyle.Exclamation, "DİKKAT")
+        Else
+            cmd.Connection = con
+            cmd.CommandText = "Update Students SET stName='" & stName.Text & "',stSurname='" & stSurname.Text & "',stNumber='" & stNumber.Text & "',stClass='" & stClass.Text & "' WHERE Id='" & Id & "' "
+            con.Open()
+            cmd.ExecuteNonQuery()
+            con.Close()
+            MsgBox("Değiştirmiş Olduğunuz Veriler Doğrultusunda Kayıtlar Güncellenmiştir.", MsgBoxStyle.Exclamation, "Değişiklik Uyarısı")
+            Call clear()
+            Call show()
+        End If
+    End Sub
+
+
+End Class
